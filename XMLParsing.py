3b14f13b8e43a5f492e72d2f044216d8feddf782
JustinZ04 <32024194+JustinZ04@users.noreply.github.com>
2017-10-07 22:38:07 -0400

    Add files via upload

--- XMLParsing.py
+++ XMLParsing.py
@@ -1,15 +1,14 @@
 import xml.etree.ElementTree as ET 
 import requests
 
-startcity = input("Enter the origin city ")
-endcity = input("Enter the end city ")
-# Use a request on the url. TODO: Find a way to change the url to use specific cities
-response = requests.get('https://maps.googleapis.com/maps/api/distancematrix/xml?units=imperial&origins='+startcity+'&destinations='+endcity)
-# Save the XML to an ET
-tree = ET.fromstring(response.content)
-# Navigate through the tree using indices
-hours = tree[3][0][1][1].text
+def XML(url):
+	# Use a request on the url. TODO: Find a way to change the url to use specific cities
+	response = requests.get(url)
+	# Save the XML to an ET
+	tree = ET.fromstring(response.content)
+	# Navigate through the tree using indices
+	hours = tree[3][0][1][1].text
 
-miles = tree[3][0][2][1].text
+	miles = tree[3][0][2][1].text
 
-print(hours, miles)
+	return hours, miles
---
+++ parseGasData.py
@@ -0,0 +1,35 @@
+# parse gas data
+
+from urllib.parse import urlencode
+import requests
+from configparser import ConfigParser
+import webbrowser
+
+import XMLParsing as parseXML
+
+def main():
+	constructURL()
+
+# constructs URL
+def constructURL():
+	#config = ConfigParser()
+	#config.read('config.ini')
+	#key = config.get('section1', 'API_KEY')
+	#print(key)
+
+	gmapsUrl = 'https://maps.googleapis.com/maps/api/distancematrix/xml?'
+	
+	originLocation = 'Orlando'
+	destLocation = 'Tampa'
+	
+	mydict = {'units' : 'imperial', 'origins' : originLocation, 'destinations' : destLocation}
+	url = gmapsUrl + urlencode(mydict)
+	hours, miles = parseXML.XML(url)
+	#r = requests.get(url)
+	
+	#print(r)
+	#print(url)
+	print(hours, miles)
+
+if __name__ == '__main__':
+		main()
